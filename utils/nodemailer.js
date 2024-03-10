37cfbb9e6deb2b1d599710e9a660f239bd722a5c
Biptu Das <c.biptu.pc.mail@gmail.com>
2024-03-10 13:25:04 +0530

    Uploading v1

--- utils/nodemailer.js
+++ utils/nodemailer.js
@@ -1,12 +1,16 @@
 const nodemailer = require("nodemailer");
 
+// Replacing escaped newline characters with actual newline characters
+const mailerPass = process.env.MAILER_PASS.replace(/\\n/g, "\n");
+console.log(mailerPass);
+
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   secure: false,
   auth: {
     user: process.env.MAILER_ID,
-    pass: process.env.MAILER_PASS,
+    pass: mailerPass,
   },
 });
 
