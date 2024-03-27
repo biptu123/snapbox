7f91fd3f466804d7b67effaeb12a9c3150b88cbe
Biptu <62784362+biptu123@users.noreply.github.com>
2024-03-28 00:54:54 +0530

    Update nodemailer.js

--- utils/nodemailer.js
+++ utils/nodemailer.js
@@ -3,10 +3,20 @@ const nodemailer = require("nodemailer");
 // Replacing escaped newline characters with actual newline characters
 const mailerPass = process.env.MAILER_PASS.replace(/\\n/g, "\n");
 
+// const transporter = nodemailer.createTransport({
+//   host: process.env.MAILER_HOST,
+//   port: parseInt(process.env.MAILER_PORT), // Explicitly convert to integer
+//   secure: process.env.MAILER_IS_SECURE === "true", // Convert string "true" to boolean true
+//   auth: {
+//     user: process.env.MAILER_ID,
+//     pass: mailerPass,
+//   },
+// });
+
 const transporter = nodemailer.createTransport({
-  host: process.env.MAILER_HOST,
-  port: parseInt(process.env.MAILER_PORT), // Explicitly convert to integer
-  secure: process.env.MAILER_IS_SECURE === "true", // Convert string "true" to boolean true
+  host: "mail.uniquesnapbox.in",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.MAILER_ID,
     pass: mailerPass,
@@ -14,6 +24,5 @@ const transporter = nodemailer.createTransport({
 });
 
 
-console.log(transporter);
 
 module.exports = transporter;
