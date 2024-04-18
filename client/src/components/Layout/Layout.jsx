cb63e10afde1ca10f187d3c16a0379f097dad29a
Biptu Das <c.biptu.pc.mail@gmail.com>
2024-04-18 18:55:28 +0530

    Whatsapp fixed

--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -28,15 +28,21 @@ const WhatsappButton = styled.a`
 const Layout = (props) => {
   const phoneNumber = "+91 7099481497";
   const message = "Hello Uniquesnapbox! ";
+  const generateWhatsappLink = () => {
+    const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+    if (isMobile) {
+      return `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
+    } else {
+      return `https://web.whatsapp.com/send?phone=${phoneNumber}&text=${encodeURIComponent(
+        message
+      )}`;
+    }
+  };
   return (
     <>
       <Header />
       <div style={{ marginTop: "100px" }}>{props.children}</div>
-      <WhatsappButton
-        href={`https://wa.me/${phoneNumber}?text=${encodeURIComponent(
-          message
-        )}`}
-      >
+      <WhatsappButton href={generateWhatsappLink()}>
         <IoLogoWhatsapp size={70} color="green" />
       </WhatsappButton>
 
